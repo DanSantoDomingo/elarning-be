14808f22d8302ece84afc4f9c41ccdf1f385fd58
Dan Sto. Domingo <danielesto.domingo@gmail.com>
2023-04-22 00:59:19 +0800

    Dockerize the djaango backend app

---
+++ docker/start_django.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Apply database migrations
+echo "start django migrations"
+
+python manage.py migrate
+
+# Collect static files
+python manage.py collectstatic --noinput
+
+# Start the server
+echo "start django"
+# gunicorn config.wsgi:application --bind 0.0.0.0:8000 --workers 2
+python manage.py runserver 0.0.0.0:8000
