d636678fc6e3e022417d5c238de4cac90a149629
Dan Sto. Domingo <danielesto.domingo@gmail.com>
2023-04-22 20:37:34 +0800

    Added whitenoise for serving static files

--- backend/urls.py
+++ backend/urls.py
@@ -20,4 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("ckeditor/", include("ckeditor_uploader.urls")),
+    path("", include("lessons.urls")),
 ]
--- docker/start_django.sh
+++ docker/start_django.sh
@@ -11,4 +11,5 @@ python manage.py collectstatic --noinput
 # Start the server
 echo "start django"
 # gunicorn config.wsgi:application --bind 0.0.0.0:8000 --workers 2
-python manage.py runserver 0.0.0.0:8000
+python manage.py runserver 0.0.0.0:8000 --nostatic
+# gunicorn backend.wsgi:application --bind 0.0.0.0:8000
