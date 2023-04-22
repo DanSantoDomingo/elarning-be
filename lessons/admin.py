3bc7f472a0614eceee20a50bcda6e13403bc13d9
Dan Sto. Domingo <danielesto.domingo@gmail.com>
2023-04-22 23:21:15 +0800

    Added models and admin controls

--- lessons/admin.py
+++ lessons/admin.py
@@ -7,14 +7,53 @@ from ckeditor.widgets import CKEditorWidget
 admin.site.site_url = None
 
 
-class LessonAdminForm(forms.ModelForm):
-    content = forms.CharField(widget=CKEditorWidget())
-    class Meta:
-        model = models.Lesson
-        fields = '__all__'
+class QuestionInline(admin.TabularInline):
+    class QuestionInlineForm(forms.ModelForm):
+        content = forms.CharField()
+        class Meta:
+            model = models.Question
+            fields = ["content", "question_type"]
 
+    form = QuestionInlineForm
+    model = models.Question
+    extra = 1
+    show_change_link = True
+
+
+class QuestionChoiceInline(admin.TabularInline):
+    class QuestionChoiceInlineForm(forms.ModelForm):
+        content = forms.CharField()
+        class Meta:
+            model = models.QuestionChoice
+            fields = ["content", "is_correct"]
+
+    form = QuestionChoiceInlineForm
+    model = models.QuestionChoice
+    extra = 1
+    show_change_link = True
+    
 
 @admin.register(models.Lesson)
 class LessonAdmin(admin.ModelAdmin):
+    class LessonAdminForm(forms.ModelForm):
+        content = forms.CharField(widget=CKEditorWidget())
+        class Meta:
+            model = models.Lesson
+            fields = '__all__'
+
     form = LessonAdminForm
     list_display = ["title", "created", "modified"]
+    inlines = [QuestionInline]
+
+
+@admin.register(models.Question)
+class QuestionAdmin(admin.ModelAdmin):
+    readonly_fields = ["lesson",]
+    fields = ["lesson", "question_type", "content", "solution",]
+    inlines = [QuestionChoiceInline]
+
+
+@admin.register(models.QuestionChoice)
+class QuestionChoiceAdmin(admin.ModelAdmin):
+    readonly_fields = ["question",]
+
--- lessons/models.py
+++ lessons/models.py
@@ -15,5 +15,20 @@ class Lesson(TimeStampedModel):
     def __str__(self) -> str:
         return self.title
 
-# class Question(models.Model):
-#     content
+class Question(models.Model):
+    class QuestionType(models.IntegerChoices):
+        MULTIPLE_CHOICE = 1
+        ESSAY = 2
+
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
+    content = RichTextUploadingField(blank=True)
+    question_type = models.IntegerField(choices=QuestionType.choices, default=QuestionType.MULTIPLE_CHOICE)
+    solution = RichTextUploadingField(blank=True)
+
+    def __str__(self) -> str:
+        return f"{self.lesson} - Question {self.id or ''}"
+
+class QuestionChoice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    is_correct = models.BooleanField(default=False)
+    content = models.CharField(max_length=256)
